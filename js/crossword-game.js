d9b8943caf4f0cb1195afa3211b3942d745aa0a7
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-02-19 12:37:47 +0100

    Added cells onclick highlighting functions

--- js/crossword-game.js
+++ js/crossword-game.js
@@ -39,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
           cell.dataset.index = cellCounter - i - 1;
           cell.appendChild(input);
+          input.addEventListener('focus', cellClickHandler);
         }
 
         row.appendChild(cell);
@@ -79,5 +80,85 @@ document.addEventListener('DOMContentLoaded', function () {
       clueElement.textContent = `${clue.designation}. ${clue.clue}`;
       cluesDownContainer.appendChild(clueElement);
     });
+  }
+
+  function cellClickHandler(event) {
+    const clickedCell = event.target;
+    const row = clickedCell.dataset.row;
+    const col = clickedCell.dataset.col;
+
+    if (row && col) {
+      console.log(`Clicked cell: row=${row}, col=${col}`);
+      resetHighlight();
+      highlightAcross(clickedCell);
+      highlightDown(clickedCell);
+    }
+  }
+
+  function resetHighlight() {
+    const crosswordTable = document.getElementsByClassName('crossword-grid');
+    const crosswordCells = crosswordTable[0].getElementsByTagName('td');
+    Array.from(crosswordCells).forEach(cell => {
+      if(!cell.classList.contains('black-cell')) {
+        cell.removeAttribute('class')
+      }
+    })
+  }
+
+  function highlightAcross(elem) {
+    let prevCell = elem.parentNode.previousSibling;
+    let nextCell = elem.parentNode.nextSibling;
+    elem.parentNode.classList.add('cell-active');
+    let startHighlightNumber = elem.parentNode.dataset.index;
+    let endHighlightNumber = elem.parentNode.dataset.index;
+    while (prevCell) {
+      prevCell.classList.add('cell-highlighted');
+      if (prevCell.classList.contains('black-cell')) {
+        break
+      } else {
+        startHighlightNumber = prevCell.dataset.index;
+        prevCell = prevCell.previousSibling;
+      }
+    }
+    while (nextCell) {
+      nextCell.classList.add('cell-highlighted');
+      if (nextCell.classList.contains('black-cell')) {
+        break
+      } else {
+        endHighlightNumber = nextCell.dataset.index;
+        nextCell = nextCell.nextSibling;
+      }
+    }
+    console.log(`Clue: ${startHighlightNumber}-${endHighlightNumber}`);
+  }
+
+  function highlightDown(elem) {
+    const currentRow = elem.dataset.col;
+    let prevRow = elem.parentNode.parentNode.previousSibling;
+    let nextRow = elem.parentNode.parentNode.nextSibling;
+    elem.parentNode.classList.add('cell-active');
+    let startHighlightNumber = elem.parentNode.dataset.index;
+    let endHighlightNumber = elem.parentNode.dataset.index;
+    while (prevRow) {
+      let prevCell = prevRow.querySelectorAll("td[data-col='" + currentRow + "']")[0];
+      prevCell.classList.add('cell-highlighted');
+      if (prevCell.classList.contains('black-cell')) {
+        break
+      } else {
+        startHighlightNumber = prevCell.dataset.index;
+        prevRow = prevRow.previousSibling;
+      }
+    }
+    while (nextRow) {
+      let prevCell = nextRow.querySelectorAll("td[data-col='" + currentRow + "']")[0];
+      prevCell.classList.add('cell-highlighted');
+      if (prevCell.classList.contains('black-cell')) {
+        break
+      } else {
+        endHighlightNumber = prevCell.dataset.index;
+        nextRow = nextRow.nextSibling;
+      }
+    }
+    console.log(`Clue: ${startHighlightNumber}-${endHighlightNumber}`);
   }
 })
